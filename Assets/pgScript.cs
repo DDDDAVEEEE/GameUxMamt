b8a2fd4bdcb7a12b3cbae22c2bb448544221de71
KonamiKodeBA <156656205+KonamiKodeBA@users.noreply.github.com>
2025-06-12 18:50:25 +0200

    mamt
    fasc

--- Assets/pgScript.cs
+++ Assets/pgScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.Scripting.APIUpdating;
 using UnityEngine.SceneManagement;
+using UnityEditor.Tilemaps;
 
 public class pgScript : MonoBehaviour
 {
@@ -76,17 +77,15 @@ public class pgScript : MonoBehaviour
         //-------------------------------------------------------------------------------------------------------------------
         //horizontal = Input.GetAxisRaw("Horizontal");
 
-
-
         if (pgIsAlive == true)
         {
             if (Input.GetKeyDown(KeyCode.W) && !isJumping)//jump
             {
-                anim.SetBool("IsJumping", true);
+                isJumping = true;
+                anim.SetBool("IsJumping", isJumping);
                 rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
 
-                isJumping = true;
-                StartCoroutine(Jump());
+
             }
             //-------------------------------------------------------------------------------------------------------------------
             if (Input.GetKeyDown(KeyCode.S) && canSlide && !isJumping) // Attiva la scivolata premendo Shift
@@ -101,48 +100,33 @@ public class pgScript : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.A) && canAttacco) // Attiva la 
             {
                 Debug.Log("Attacco avviato!");
-                //canShild = false;
-                //canAttacco = true;
-
-                
                 StartCoroutine(Attacco());
-
-                //canAttacco = false;
-                //canShild = true;        
             }
             //-------------------------------------------------------------------------------------------------------------------   
             if (Input.GetKeyDown(KeyCode.D) && canShild) // Attiva la 
             {
                 Debug.Log("Scudo avviato!");
-                //canAttacco = false;
-                //canShild = true;
-
-                
                 StartCoroutine(Shild());
-
-                //canAttacco = true;
-                //canShild = false;
-
             }
 
-            transform.position += new Vector3(movimento * depthFactor * Time.deltaTime, 0, 0);
             if (!canShild && !canAttacco && !canSlide)
             {
-                anim.SetBool("IsRunning", true);
                 /* anim.SetBool("IsShield", false);
                  anim.SetBool("IsAttacco", false);
                  anim.SetBool("IsSlide", false);
                  anim.SetBool("IsJumping", false);*/
 
             }
-            else
-            {
-                anim.SetBool("IsRunning", false);
-
-            }
 
 
         }
+    }
+
+    private void FixedUpdate()
+    {
+        anim.SetFloat("yVelocity", rb.linearVelocityY);
+        if(pgIsAlive) transform.position += new Vector3(movimento * depthFactor * Time.deltaTime, 0, 0);
+    }
         /*else
         {
             //animazione vittoria/idle
@@ -157,7 +141,6 @@ public class pgScript : MonoBehaviour
 
             //-------------------------------------------------------------------------------------------------------------------
 
-    }
     /* void FixedUpdate()
      { 
          rb.linearVelocity = new Vector2(speed, rb.linearVelocity.y);
@@ -173,50 +156,55 @@ public class pgScript : MonoBehaviour
         due.gameObject.SetActive(false);
         uno.gameObject.SetActive(true);
         yield return new WaitForSeconds(1);
-        anim.SetBool("IsIdle", false);
         uno.gameObject.SetActive(false);
         go.gameObject.SetActive(true);
         yield return new WaitForSeconds(1);
+        anim.SetBool("IsIdle", false);
         go.gameObject.SetActive(false);
         pgIsAlive = true;
-    }
-
-    IEnumerator Jump()
-    {
-        anim.SetBool("IsJumping", true);
-        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
-        isJumping = true;
-        yield return new WaitForSeconds(0.5f);
-        anim.SetBool("IsJumping", true);
     }
     
     IEnumerator Shild()
     {
         Debug.Log("funziona!");
-        anim.SetBool("IsShield", true);
+        anim.SetBool("IsShield", canShild);
         canShild = false;
         canAttacco = false;
+        canSlide = false;
         hitboxS.gameObject.SetActive(true);
-        yield return new WaitForSeconds(0.5f); // Aspetta il tempo di ricarica
+        yield return new WaitForSeconds(2f); // Aspetta il tempo di ricarica
         anim.SetBool("IsShield", false);
+        yield return new WaitForSeconds(0.5f); // Aspetta il tempo di ricarica
         hitboxS.gameObject.SetActive(false);
         canAttacco = true;
         canShild = true;
+        canSlide = true;
+    }
+    IEnumerator Pop()
+    {
+        anim.SetBool("IsShield", false);
+        yield return new WaitForSeconds(0.5f); // Aspetta il tempo di ricarica
+        hitboxS.gameObject.SetActive(false);
+        canAttacco = true;
+        canShild = true;
+        canSlide = true;
     }
     //------------------------------------------------------------------------------------------------------------------------
 
     IEnumerator Attacco()
     {
         Debug.Log("funziona attacco!");
-        anim.SetBool("IsAttacco", true);
+        anim.SetBool("IsAttacco", canAttacco);
         canAttacco = false;
         canShild = false;
+        canSlide = false;
         hitboxA.gameObject.SetActive(true);
         yield return new WaitForSeconds(0.5f); // Aspetta il tempo di ricarica
         anim.SetBool("IsAttacco", false);
         hitboxA.gameObject.SetActive(false);
         canAttacco = true;
         canShild = true;
+        canSlide=true;
     }
     IEnumerator Slide()
     {
@@ -228,21 +216,24 @@ public class pgScript : MonoBehaviour
         
         rb.linearVelocity = slideDirection * slideSpeed; */
         anim.SetBool("IsSlide", true);
-        if(jump)
-        bc.size += new Vector2(0, -2);
+        bc.size += new Vector2(+2f, -2f);
         canSlide = false;
+        canShild = false;
+        canAttacco = false;
         Debug.Log("Inizio scivolata per " + slideDuration + " secondi");
         yield return new WaitForSeconds(slideDuration); // Aspetta la durata dello 
         anim.SetBool("IsSlide", false);
         Debug.Log("Fine scivolata");
         GetComponent<RectTransform>().position += new Vector3(0, 1, 0);
-        bc.size += new Vector2(0, 2);
+        bc.size += new Vector2(-2f, 2f);
         /* rb.linearVelocity = Vector2.zero; // Ferma il movimento
         rb.gravityScale = originalGravity; // Ripristina la gravità */
 
         yield return new WaitForSeconds(cooldownTime); // Aspetta il tempo di ricarica
         Debug.Log("Fine cooldown");
         canSlide = true; // Permette di scivolare di nuovo
+        canAttacco=true;
+        canShild=true;
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -250,6 +241,7 @@ public class pgScript : MonoBehaviour
         if (collision.gameObject.tag == "ground")
         {
             isJumping = false;
+            anim.SetBool("IsJumping", isJumping);
         }
         if (collision.gameObject.tag == "DannoGr" && !imm)
         {
@@ -259,21 +251,15 @@ public class pgScript : MonoBehaviour
         if (collision.gameObject.tag == "mob" && !imm)
         {
             Debug.Log("prende danno!");
-            if (collision.gameObject.tag == "Attacco")
-                Debug.Log("STA ATTACCANDO NON SUBISCE DANNO!");
-            else
-            {
+            if (canAttacco)
                 StartCoroutine(Damage());
-            }
         }
         if (collision.gameObject.tag == "Proie" && !imm)
         {
-            if (collision.gameObject.tag == "Shild")
-                Debug.Log("STA ATTACCANDO NON SUBISCE DANNO!");
-            else
-            {
+            if (canShild)
                 StartCoroutine(Damage());
-            } 
+            else
+                Pop();
         }
     }
     public void OnTriggerEnter2D(Collider2D collider)
@@ -281,7 +267,6 @@ public class pgScript : MonoBehaviour
         Debug.Log("idk entra funzione con" + collider.gameObject.tag);
         if (collider.gameObject.tag == "GameOver")
         {
-
             GameOver();
         }
         if ((collider.gameObject.tag == "DannoGr" || collider.gameObject.tag == "Proie") && !imm)
